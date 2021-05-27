9f7102de7032974ff433508626a5fd4c1a8eeec9
tajveez <tajveez786@gmail.com>
2021-05-27 23:14:06 +0500

    Code

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,10 @@
+import NewMeetupForm from '../../components/home-page/hero'
+const NewMeetupPage = () => {
+    return (
+        <div>
+            <NewMeetupForm />
+        </div>
+    )
+}
+
+export default NewMeetupPage
