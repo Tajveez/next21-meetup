edf48c2dcbe5b9fcf8404d486fa0e92552dfa883
tajveez <tajveez786@gmail.com>
2021-05-30 23:37:22 +0500

    fragment added

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Fragment } from 'react'
+
+const MeetupDetails = () => {
+    return (
+        <Fragment>
+            <image src="" alt=""/>
+        </Fragment>
+    )
+}
+
+export default MeetupDetails
